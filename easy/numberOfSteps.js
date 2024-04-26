380cbab403a9af3fb387aacaab30e451260ba4ac
kirakos <henacodes@gmail.com>
2024-04-26 09:38:08 +0300

    number of steps

---
+++ easy/numberOfSteps.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function (num) {
+  let currentNumber = num;
+  let steps = 0;
+
+  while (currentNumber > 0) {
+    if (currentNumber % 2 == 0) {
+      currentNumber = currentNumber / 2;
+    } else {
+      currentNumber = currentNumber - 1;
+    }
+    steps += 1;
+  }
+
+  return steps;
+};
+
+console.log(numberOfSteps(14));
